b9ea909aa8e4e69369fa1156c727db33f40ecfde
samuelrafini <Samuelrafini@gmail.com>
2018-02-10 14:28:16 +0100

    Rendering Templates with Data

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express= require('express');
+const hbs = require('hbs');
+
 var app = express();
 
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 app.get('/',(req, res)=> {
     // res.send('<h1>Hello Express!<h1>');
-    res.send({
-        name: 'Samuel RAfini',
-        likes: ['Music', 'Javascript']
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome Home'
     })
 });
 
 app.get('/about',( req, res) => {
-    res.send('About Page');
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+   });
 });
 
 app.get('/bad', (req, res) => {
@@ -25,3 +32,4 @@ app.get('/bad', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
+
