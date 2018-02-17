78f31568b2e08ed10e75caa663f88ffa439a7740
samuelrafini <Samuelrafini@gmail.com>
2018-02-17 10:10:35 +0100

    Adding andrew's mead files

--- server.js
+++ server.js
@@ -6,60 +6,45 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server.log.')
-    }
-  });
+  fs.appendFile('server.log', log + '\n');
   next();
 });
 
-//comment below when not in maintenance
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-});
-
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-app.get('/', (req, res) => {
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
+app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
-  //res.send('About Page');
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
-app.get('/projects', (req, res) => {
-  //res.send('About Page');
-  res.render('projects.hbs', {
-    pageTitle: 'Projects',
-  });
-});
-
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
@@ -67,6 +52,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is up and on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
-
